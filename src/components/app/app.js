eeb81b2a92b492ec1be5742404568da730396164
Dima <example@gmail.com>
2026-01-06 23:39:55 +0100

    dynamic list items with props created

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -7,6 +7,13 @@ import EmployersAddForm from '../employers-add-form/employers-add-form';
 import './app.css';
 
 function App() {
+
+	const data = [
+		{name: 'John C.', salary: 800, increase: true},
+		{name: 'Alex M.', salary: 3000, increase: true},
+		{name: 'Carl W.', salary: 5000, increase: false},
+	];
+
 	return (
 		<div className="app">
 			<AppInfo />
@@ -16,7 +23,7 @@ function App() {
 				<AppFilter />
 			</div>
 
-			<EmployersList />
+			<EmployersList data={data} />
 			<EmployersAddForm />
 		</div>
 	);
--- src/components/employers-list/employers-list.js
+++ src/components/employers-list/employers-list.js
@@ -2,12 +2,14 @@ import EmployersListItem from '../employers-list-item/employers-list-item';
 
 import './employers-list.css';
 
-const EmployersList = () => {
+const EmployersList = ({data}) => {
+
+	const elements = data.map(item => {
+		return <EmployersListItem {...item} />
+	})
 	return (
 		<ul className="app-list list-group">
-			<EmployersListItem />
-			<EmployersListItem />
-			<EmployersListItem />
+			{elements}
 		</ul>
 	)
 };
