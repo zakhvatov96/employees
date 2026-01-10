59eb5095ac0d7fe9684266768561694dff826c36
Dima <example@gmail.com>
2026-01-10 14:55:19 +0100

    increase and like added

--- src/components/employers-list/employers-list.js
+++ src/components/employers-list/employers-list.js
@@ -5,7 +5,8 @@ import './employers-list.css';
 const EmployersList = ({data}) => {
 
 	const elements = data.map(item => {
-		return <EmployersListItem {...item} />
+		const {id, ...itemProps} = item;
+		return <EmployersListItem key={id} {...itemProps} />
 	})
 	return (
 		<ul className="app-list list-group">
